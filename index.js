0634758286f28b7bfd2d0774f6d8b4edf6be8cc9
Rubyroy12 <ibrakipz7@gmail.com>
2021-03-14 01:52:58 +0300

    it will add the js function to calculate total production

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-  
-        var k = "The respective values are : <br>"; 
-        
-        function data() { 
-            var input = document.getElementsByName('array[]'); 
-  
-            for (var i = 0; i < input.length; i++) { 
-                var a = input[i]; 
-                k = k +  a.value + " " +"<br>"; 
-            } 
+var k = "The respective values are : <br>";
 
-            document.getElementById("par").innerHTML = k; 
-            // document.getElementById("po").innerHTML = "Output"; 
-        }
+function data() {
+    var record = document.getElementsByName('sheds');
+
+    for (var i = 0; i < record.length; i++) {
+        var a = record[i];
+        k = k + a.value + " " + "<br>";
+
+
+    }
+
+    document.getElementById("par").innerHTML = k;
+    // document.getElementById("po").innerHTML = "Output"; 
+}
+
+
+// adding elements in the array
+
+function total_production(input) {
+    
+    var sum = 0;
+    for (var i = 0; i < input.length; i++) {
+        sum = sum + input[i];
+    }
+    document.getElementById("total").innerHTML = sum;
+}
+var input = document.getElementsByName('sheds');
