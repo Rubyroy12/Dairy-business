6e63fdee2bce17db9c05fcca58dd89ff6a8cc1e5
Rubyroy12 <ibrakipz7@gmail.com>
2021-03-13 22:16:17 +0300

    it will add html form to get user input

---
+++ index.js
@@ -0,0 +1,14 @@
+  
+        var k = "The respective values are : <br>"; 
+        
+        function data() { 
+            var input = document.getElementsByName('array[]'); 
+  
+            for (var i = 0; i < input.length; i++) { 
+                var a = input[i]; 
+                k = k +  a.value + " " +"<br>"; 
+            } 
+
+            document.getElementById("par").innerHTML = k; 
+            document.getElementById("po").innerHTML = "Output"; 
+        }
