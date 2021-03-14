2a6ec5db8e87d80d9aac28a42601cc419d0b7c70
Rubyroy12 <ibrakipz7@gmail.com>
2021-03-15 01:29:42 +0300

    it will delete thenavigation bar in html

--- index.js
+++ index.js
@@ -1,21 +1,70 @@
 var sheds = [{
-  name:"A",
-  litres: 510,
-},
-{
-  name:"B",
-  litres:308,
-},
-{
-  name:"C",
-  litres: 486,
-},
-{
-  name:"D",
-  litres: 572,
-}];
+    name: "A",
+    litres: 510,
+  },
+  {
+    name: "B",
+    litres: 308,
+  },
+  {
+    name: "C",
+    litres: 486,
+  },
+  {
+    name: "D",
+    litres: 572,
+  }
+];
 
 
-for(var i =0; i<sheds.length; i++){
-  document.write("Your daily production in shed " + sheds[i].name + "is "+sheds[i].litres + "<br>")
+
+var k   ; 
+function data() { 
+    var input = document.getElementsByName('sheds'); 
+
+    for (var i = 0; i < input.length; i++) { 
+        var a = input[i]; 
+        k = k +  a.value + " " +"<br>"; 
+    } 
+    var sheds =["A", "B", "C", "D"];
+for (var n = 0; n < sheds.length; n++) {
+  alert("Your daily production in shed " + sheds[n] + " is  " + k);
+}
+    
+    document.getElementById("output").innerHTML = k; 
+    // document.getElementById("po").innerHTML = "Output"; 
+} 
+
+function totalproduction() {
+    var input = document.getElementById('sheds');
+    sum = input[0].sheds + input[1].sheds + input[2].sheds + input[3].sheds;
+    alert("Your total production is " + sum + " litres  per day")
+  
+    document.getElementById("production").innerHTML = sum;
+
+ }
+
+function income_overtime() {
+  let daily_income = sum * 45;
+  let weekly_income = daily_income * 7;
+  let annual_income = daily_income * 366;
+  document.write("Your daily income is " + daily_income + " kenya shillings" + "<br>");
+  document.write("Your weekly income is " + weekly_income + " kenya shillings" + "<br>");
+  document.write("Your annual income is " + annual_income + " kenya shillings"+ "<br>");
+
+  
+  var months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var days_of_month= [ 31,29,31,30,31,30,31,31,30,31,30,31];
+  
+  for (var i = 0; i<months.length; i++){
+    sum = days_of_month[i] * daily_income;
+    document.write("Your income for " + months[i] + " is " + sum + "kenyan shillings" + "<br>");
+    
+
+    
+
+  }
+ document.getElementById("income").innerHTML = daily_income;
+
+  
 }
